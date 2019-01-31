0fc4e51a3b275afb7eaaf99e10b16770a8e0219b
Christian Schweigel <christian.david.schweigel@sap.com>
2019-01-31 08:46:29 +0100

    Changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Bencher',
-      version='0.2.3',
+      version='0.2.4',
       description='Benchmarking Suite',
       author='Christian Schweigel',
       author_email='',
