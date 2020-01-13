4cde056e7a4707324ce963f2ae56ad91bf4535aa
Christian Schweigel <christian.david.schweigel@sap.com>
2020-01-13 10:39:57 +0100

    extend setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='SimpleBencher',
       version='0.6.0',
       description='Benchmarking Suite',
       author='Christian Schweigel',
       author_email='',
-      url='',
-      packages=['bencher'],
+      url='https://github.com/swip3798/Bencher',
+      packages=['simple_bencher'],
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
+          "Operating System :: OS Independent",
+          "Development Status :: 3 - Alpha"
+      ]
      )
